30afd52df40dbb7a4ce32623a8c681b3d9d63e1b
Skarlit <rxxxie@gmail.com>
2014-05-22 17:43:53 -0700

    fixing more heroku

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -13,6 +13,6 @@
 #
 
 class Comment < ActiveRecord::Base
-  has_one :author, class_name: "User", foreign_key: user_id
+  has_one :author, class_name: "User", foreign_key: :user_id
   belongs_to :item
 end
