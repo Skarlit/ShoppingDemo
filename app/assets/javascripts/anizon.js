51a9e28c294a4be3e2c489ca7c54a4ab64c2516b
Skarlit <rxxxie@gmail.com>
2014-05-30 13:22:29 -0700

    background bug fixed

--- app/assets/javascripts/anizon.js
+++ app/assets/javascripts/anizon.js
@@ -31,8 +31,11 @@ $(document).ready(function(){
 
   window.Anizon.initialize();
   window.persistentLogin(); 
-  $.backstretch("http://cdn.wallwuzz.com/uploads/vector-wallpaper-patterns-black-wallpapers-array-wallwuzz-hd-wallpaper-7577.png");
-
+  if($.cookie("wall"))
+    $.backstretch($.cookie("wall"));
+  else{ 
+    $.backstretch("http://cdn.wallwuzz.com/uploads/vector-wallpaper-patterns-black-wallpapers-array-wallwuzz-hd-wallpaper-7577.png");
+  }
   $("#cart").qtip({
     content: {
       text: 'Click Here to Create/Toggle Cart'
